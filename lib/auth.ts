09004f1a344b65bab84479aa26b5a0aaa1129562
sunfeax <sunfeax@gmail.com>
2026-01-12 13:42:13 +0100

    feat: integrate better-auth for user authentication and add user/session models in Prisma schema

---
+++ lib/auth.ts
@@ -0,0 +1,9 @@
+import { prisma } from '@/db/prisma';
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql",
+    }),
+});
