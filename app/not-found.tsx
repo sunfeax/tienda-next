b094ec84ab12e6137b6a7c17c857a6145f6b558a
sunfeax <sunfeax@gmail.com>
2026-01-26 14:44:40 +0100

    feat: update NotFound component to use correct logo path and improve layout

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,8 +6,10 @@ import { Button } from "@/components/ui/button";
 const NotFound = () => {
   return (
     <div className="flex flex-col border border-slate-100 items-center justify-center min-h-screen">
-      <Image src={"../logo.svg"} alt="logo" width={48} height={48} priority/>
       <div className="p-6 rounded-xl border border-gray-100 shadow-2xl text-center bg-white">
+        <div className="mx-auto mb-4 flex h-12 w-12 items-center justify-center rounded-lg bg-amber-400">
+          <Image src="/logo.svg" alt="logo" width={28} height={28} priority />
+        </div>
         <h1 className="text-3xl font-bold mb-4">Ooops...</h1>
         <p className="text-destructive">The request page could not be found.</p>
         <Button
